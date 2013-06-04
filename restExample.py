6245cd7f2fe7024f821aa9bfee86ae15cfdd5048
yaoml <yaominglei@gmail.com>
2013-06-04 11:02:12 +0800

    rest接口例子

--- restExample.py
+++ restExample.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-@app.route('about')
+@app.route('/about/')
 def about_us():
     return render_template('about.html')
 
